b8236f662b2990e516a6fed4cc50dc5287d5b24a
Lqwd <AppLoneQW@yandex.ru>
2025-09-13 16:40:47 +0300

    Добавил AuthConfig

---
+++ src/main/java/dev/lqwd/configuration/AuthConfig.java
@@ -0,0 +1,38 @@
+package dev.lqwd.configuration;
+
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@PropertySource("classpath:app.properties")
+@Setter
+@Getter
+public class AuthConfig {
+
+    @Value("${webMvc.corsRegistry.applicationBasePath}")
+    private String applicationBasePath;
+
+    private String baseUrl;
+    private String homeUrl;
+    private String signInUrl;
+    private List<String> publicUrls;
+
+    @PostConstruct
+    public void init() {
+        this.baseUrl = "/" + applicationBasePath;
+        this.homeUrl = baseUrl + "/home";
+        this.signInUrl = baseUrl + "/sign-in";
+        this.publicUrls = Arrays.asList(
+                baseUrl + "/",
+                signInUrl,
+                baseUrl + "/sign-up"
+        );
+    }
+}
